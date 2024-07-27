f38b2d2fd1ec4c1092518447f1239145011ff8e9
Binod Rasaili <binod.toyam@gmail.com>
2024-07-26 22:42:26 -0500

    add CORS to allow localhost:3000 to access all the controller

--- src/main/java/flight_booking/controller/DemoController.java
+++ src/main/java/flight_booking/controller/DemoController.java
@@ -1,12 +1,14 @@
 package flight_booking.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
+@CrossOrigin(origins = "http://localhost:3000")
 public class DemoController {
 
 
--- src/main/java/flight_booking/securityConfig/SecurityConfiguration.java
+++ src/main/java/flight_booking/securityConfig/SecurityConfiguration.java
@@ -4,11 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -31,6 +34,7 @@ public class SecurityConfiguration {
                         // Any other request must be authenticated
                         .anyRequest().authenticated()
                 )
+                .cors(withDefaults())
 
                 .sessionManagement(sessionManagement -> sessionManagement
                         // Configure session management to be stateless
