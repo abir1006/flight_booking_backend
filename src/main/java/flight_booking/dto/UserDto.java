27cb6d5807187f38e4ea62be8da1720625ca1a5f
Binod Rasaili <binod.toyam@gmail.com>
2024-08-03 17:01:59 -0500

    user dto fixed

--- src/main/java/flight_booking/dto/UserDto.java
+++ src/main/java/flight_booking/dto/UserDto.java
@@ -1,6 +1,7 @@
 package flight_booking.dto;
 
 
+import flight_booking.domain.Role;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,9 +12,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class UserDto {
+    private long id;
     private String firstname;
     private String lastname;
     private String email;
     private String phone;
     private String address;
+    private Role role;
 }
