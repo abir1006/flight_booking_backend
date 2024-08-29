5fae904cbf36ba98b8c63b6ebcf081f584034f7f
Abdoon Nur <abir023048@gmail.com>
2024-08-29 08:10:10 -0500

    Dockerfile added

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
 FROM openjdk:22-jdk-slim
 EXPOSE 8080
 ADD target/flight_booking-0.0.1-SNAPSHOT.jar flight_booking-0.0.1-SNAPSHOT.jar
-ADD env.properties env.properties
 ENTRYPOINT ["java","-jar","/flight_booking-0.0.1-SNAPSHOT.jar"]
--- src/main/java/flight_booking/config/WebConfig.java
+++ src/main/java/flight_booking/config/WebConfig.java
@@ -1,5 +1,6 @@
 package flight_booking.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,13 +9,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig {
 
+    @Value("${CORS_URL}")
+    private String allowedOrigin;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins(allowedOrigin)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
